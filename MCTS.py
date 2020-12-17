796eb07616fb8bf4dd59da869ecadf7fdc104760
Ruben Schenkhuizen <ruben.schenkhuizen@gmail.com>
2020-12-17 17:58:17 +0100

    Basic implementation of MCTS is finished.

--- MCTS.py
+++ MCTS.py
@@ -1,18 +1,120 @@
+from random import random
 from binary_tree import Tree
+import math
+import numpy as np
 
 
 class MCTS():
-    def __init__(self):
-        print("Hello")
+    def __init__(self, c, d, max_iters, max_rollouts, nr_iters):
+        self.c = c
+        self.d = d
+        self.max_iters = max_iters
+        self.max_rollouts = max_rollouts
+        self.nr_iters = nr_iters
+
+        num_leaves = 2 ** self.d
+
+        self.tree = Tree()
+        for _ in range(num_leaves):
+            self.tree.insert()
+
+        self.tree.assign_vals_to_leafs(num_leaves)
+
+    def compute_UCB(self, node_key):
+
+        if node_key not in self.tree.data:
+            return math.inf
+
+        t = self.tree.data[node_key].t
+        n = self.tree.data[node_key].n
+
+        parent = self.tree.get_parent(node_key)
+        visit_parent = self.tree.data[parent].n
+
+        if n > 0:
+            return t/n + self.c * math.sqrt((math.log(visit_parent)/n))
+        else:
+            return math.inf
+
+    # Perform the tree policy and construct a path from the root to the most promising leaf node (with highest finite UCB).
+    # Return the snowcap leaf with the highest UCB.
+    def select_node(self):
+        # Always start at the root
+        node_key = 0
+        temp_node = node_key
+        path = []
+
+        # The root
+        if self.tree.data[node_key].n == 0:
+            path.append(node_key)
+            return node_key, path
+
+        # Larger than 0 to prevent computing UCB values on leaves.
+
+        while(node_key in self.tree.data and self.tree.data[node_key].n > 0):
+            temp_node = node_key
+            path.append(node_key)
+            left = self.tree.get_left_child(node_key)
+            right = self.tree.get_right_child(node_key)
+
+            ucb_left = self.compute_UCB(left)
+            ucb_right = self.compute_UCB(right)
+
+            if ucb_left > ucb_right:
+                node_key = left
+            elif ucb_left < ucb_right:
+                node_key = right
+            else:
+                random_val = np.random.uniform()
+                if random_val > 0.5:
+                    node_key = left
+                else:
+                    node_key = right
+
+        return temp_node, path
+
+    def expand_node(self, node_key):
+        random_val = np.random.uniform()
+        # Pick left unexplored child
+        if random_val > 0.5:
+            left = self.tree.get_left_child(node_key)
+            return left
+
+        else:
+            right = self.tree.get_right_child(node_key)
+            return right
+
+    # Do a rollout to some leaf node at the bottom of the tree
+
+    def rollout(self, node_key):
+        temp_node = node_key
+        while(node_key in self.tree.data):
+            temp_node = node_key
+            node_key = self.expand_node(node_key)
+
+        return self.tree.data[temp_node].t / self.tree.data[temp_node].n
+
+    # Backup the values from the unexplored node to the root (reverse tree policy path)
+
+    def backup(self, node_key, path, reward):
+        path.append(node_key)
+        backup_path = path[::-1]
+
+        for backup_key in backup_path:
+            self.tree.data[backup_key].t += reward
+            self.tree.data[backup_key].n += 1
+
+    def perform_iters(self):
+        for _ in range(self.nr_iters):
+            node_key, path = self.select_node()
+            expand_node_key = self.expand_node(node_key)
+            if expand_node_key in self.tree.data and self.tree.data[expand_node_key].n >= 0:
+                rollout_reward = self.rollout(expand_node_key)
+                self.backup(expand_node_key, path, rollout_reward)
 
 
 if __name__ == "__main__":
-    d = 4
-    total = 2**d
-    tree = Tree()
-
-    for i in range(total):
-        tree.insert()
-
-    tree.assign_vals_to_leafs(total)
-    tree.debug_tree()
+    mcts = MCTS(2, 3, 50, 5, 100)
+    mcts.perform_iters()
+    mcts.tree.pp_tree(0, 0)
+    mcts.tree.debug_tree()
--- binary_tree.py
+++ binary_tree.py
@@ -11,58 +11,73 @@ class Node:
 
         self.t = 0
         self.n = 0
-        self.val = 0
-
-        # self.left = None
-        # self.right = None
 
 
 class Tree:
     def __init__(self) -> None:
-        self.tree = {}
+        self.data = {}
         self.leaves = set()
         self.root = None
 
     def get_root(self):
-        if self.tree:
-            return self.tree[0]
+        if self.data:
+            return self.data[0]
         else:
             return None
 
+    def get_parent(self, node_key):
+        # Right child has even node key
+        if node_key % 2 == 0:
+            parent = (node_key - 2) / 2
+        else:
+            parent = (node_key - 1) / 2
+
+        return parent
+
     def get_left_child(self, node_key):
-        return self.tree[node_key * 2 + 1]
+        return node_key * 2 + 1
 
     def get_right_child(self, node_key):
-        return self.tree[node_key * 2 + 2]
+        return node_key * 2 + 2
 
     def insert(self):
         if not self.get_root():
-            self.tree[0] = Node()
+            self.data[0] = Node()
             self.leaves.add(0)
         else:
             # Take an arbitrary node (leaf) and give it children
             node_key = self.leaves.pop()
             left = node_key * 2 + 1
             right = node_key * 2 + 2
-            self.tree[left] = Node()
-            self.tree[right] = Node()
+            self.data[left] = Node()
+            self.data[right] = Node()
 
             self.leaves.add(left)
             self.leaves.add(right)
 
     def debug_tree(self):
-        for key, node in self.tree.items():
-            node_val = node.val
+        for key, node in self.data.items():
             node_t = node.t
             node_n = node.n
 
-            print(f"Node {key} with value {node_val}")
-            print(f"The value of t is {node_t}")
-            print(f"The value of n is {node_n}")
+            if node_n == -1 or key == 0:
+                print(f"Node {key} with value {node_t/node_n}")
+
+    def pp_tree(self, node_key, level):
+        if node_key >= 0 and node_key in self.data:
+            left = self.get_left_child(node_key)
+            right = self.get_right_child(node_key)
+            self.pp_tree(right, level + 1)
+            print(' ' * 6 * level + '->',
+                  f"{node_key}, {self.data[node_key].t}, {self.data[node_key].n}")
+            self.pp_tree(left, level+1)
 
     def assign_vals_to_leafs(self, total_leafs):
-        vals = np.random.uniform(0, 100, total_leafs)
+        # TODO: change this to real numbers.
+        # vals = np.random.uniform(0, 100, total_leafs)
+        vals = np.random.randint(0, 100, total_leafs)
         temp_leaves = self.leaves.copy()
         for random_val in vals:
             leaf = temp_leaves.pop()
-            self.tree[leaf].val = random_val
+            self.data[leaf].t = -random_val
+            self.data[leaf].n = -1
