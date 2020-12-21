d1b02dc42ccc0d3965c0e0dbe601b49084f44b4c
Ruben Schenkhuizen <ruben.schenkhuizen@gmail.com>
2020-12-21 17:38:35 +0100

    Almost finished exercise 2.1

--- exercise_2/game.py
+++ exercise_2/game.py
@@ -1,10 +1,16 @@
 import numpy as np
 from cell import Cell, SpecialCell
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 def main():
+    dims = 9
+    nr_episodes = 1000
+
     # Construct the grid
-    rows, cols = (9, 9)
+    rows, cols = (dims, dims)
     grid = [[Cell(True) for i in range(cols)] for j in range(rows)]
     unreachable = [(2, 3), (2, 4), (2, 5), (2, 6), (2, 7), (3, 7),
                    (4, 7), (5, 7), (6, 7), (8, 2), (8, 3), (8, 4), (8, 5)]
@@ -22,12 +28,29 @@ def main():
     # Treasure
     grid[8][8] = SpecialCell(True, 50)
 
-    mc_policy_eval((0, 0), 1, grid)
+    # TODO: fix bug in random states
+    states = mc_policy_eval(nr_episodes, grid, True, dims)
+    reward_grid = np.resize(list(states.values()), (dims, dims))
+    ax = sns.heatmap(reward_grid, annot=True, fmt=".2f")
+    plt.xticks(np.arange(1, 10))
+    ax.set_xticks(np.arange(0.5, 9))
+    xlabels = [int(x + 0.5) for x in ax.get_xticks()]
+    ylabels = [int(y + 0.5) for y in ax.get_yticks()]
+    ax.set_xticklabels(xlabels)
+    ax.set_yticklabels(ylabels)
+
+    print(ax.get_yticks())
+
+    # print(states)
+    # print(reward_grid)
+
+    plt.show()
 
 
 def generate_episode(starting_state, grid):
     episode = []
     temp_state = starting_state
+    cur_state = starting_state
     row, col = temp_state
     while not isinstance(grid[row][col], SpecialCell):
         action_choice = np.random.uniform()
@@ -62,33 +85,43 @@ def generate_episode(starting_state, grid):
 # TODO: implement first-visit/every-visit MC.
 
 
-def mc_policy_eval(starting_state, nr_episodes, grid):
-    total_rewards = []
-    states = {(i, j): 0 for i in range(9) for j in range(9)}
+def mc_policy_eval(nr_episodes, grid, is_random, dims, starting_state=(0, 0)):
+    episode_counts = {(i, j): 0 for i in range(dims) for j in range(dims)}
+    states_values = {(i, j): 0 for i in range(dims) for j in range(dims)}
+    states_indices = np.arange(dims*dims)
+    states = list(states_values.keys())
+
     for _ in range(nr_episodes):
+        # Pick random starting state
+        if is_random:
+            starting_state_index = np.random.choice(states_indices)
+            starting_state = states[starting_state_index]
+
         episode, cur_state = generate_episode(starting_state, grid)
         # Reverse the episode and start from aback
         row, col = cur_state
         episode = episode[::-1]
-
-        print(len(episode))
         accum_reward = 0
 
         for dir in episode:
             accum_reward += grid[row][col].reward
             if dir == "r":
                 # Then we need to go left on the grid, because we accumulate in a tailwise
-                cur_state = (row, col-1)
+                cur_state = (row, col - 1)
             elif dir == "l":
-                cur_state = (row, col+1)
+                cur_state = (row, col + 1)
             elif dir == "u":
                 cur_state = (row + 1, col)
             elif dir == "d":
                 cur_state = (row - 1, col)
 
-            states[cur_state] = accum_reward
+            states_values[cur_state] += accum_reward
+            episode_counts[cur_state] += 1
 
             row, col = cur_state
+    states_values = {k: v/episode_counts[k] if episode_counts[k]
+                     != 0 else v for k, v in states_values.items()}
+    return states_values
 
 
 if __name__ == "__main__":
